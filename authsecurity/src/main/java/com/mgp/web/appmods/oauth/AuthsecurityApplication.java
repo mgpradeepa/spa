e8eed094fa3e3b447735abf0a55642059630c460
pradeepmg <mgpradeepa@gmail.com>
2022-05-08 15:25:45 +0530

    Initiating oauth support with gateway

---
+++ authsecurity/src/main/java/com/mgp/web/appmods/oauth/AuthsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mgp.web.appmods.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthsecurityApplication.class, args);
+	}
+
+}
---
+++ authsecurity/src/test/java/com/mgp/web/appmods/oauth/AuthsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgp.web.appmods.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
