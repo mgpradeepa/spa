1c39c8c751f03b58514ce57ebcd95e7c4b083ab1
mgpradeepa <mgpradeepa@gmail.com>
2022-12-14 03:38:17 +0530

    TS1: cleanup and java17 verification

---
+++ prodcategories/src/test/java/com/mgp/web/categories/CategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgp.web.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
